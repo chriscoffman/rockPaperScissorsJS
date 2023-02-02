ef0da9b19e5caed94f9ee7e799fc44ec714565bb
chriscoffman <chris.coffman2@gmail.com>
2023-02-02 17:05:42 -0600

    add ui for the game, feedback is displayed on page

--- javascript.js
+++ javascript.js
@@ -17,18 +17,42 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
     const outcome = (+playerSelection) - (+computerSelection);
     if (outcome  === 0) {
-        console.log("It is a tie!");
-        return 0;
+        loss_or_win_line.textContent = "It is a tie!"
     } else if (outcome === -2 || outcome === 1) {
-        console.log(`You win! ${gameOptions[playerSelection]} beats ${gameOptions[computerSelection]}`);
-        return 1;
+        loss_or_win_line.textContent = `You won! ${gameOptions[playerSelection]} beats ${gameOptions[computerSelection]}!`
+        score += 1;
+        score_content.textContent = `The score is ${score}`;
+        if (score > 4){
+            alert("You have beaten a simple machine, congratulations!");
+            score = 0;
+            score_content.textContent = `The score is ${score}`;
+        }
     } else {
-        console.log(`You lose! ${gameOptions[computerSelection]} beats ${gameOptions[playerSelection]}`);
-        return -1;
-    }
+        loss_or_win_line.textContent = `You lose! ${gameOptions[computerSelection]} beats ${gameOptions[playerSelection]}`;
+        score -= 1;
+        score_content.textContent = `The score is ${score}`;
+        if (score < -4){
+            alert("You have lost to a simple machine, congratulations!");
+            score = 0;
+            score_content.textContent = `The score is ${score}`;
+        }
+        }
 }
+const container = document.querySelector('#container');
+let score = 0;
+const score_content = document.createElement('div');
+score_content.textContent = `The score is ${score}`;
+const loss_or_win_line = document.createElement('div');
+container.appendChild(loss_or_win_line);
+container.appendChild(score_content);
+const myBtnList = document.querySelectorAll('button');
+myBtnList.forEach(button => button.addEventListener('click',
+() => playRound(button.id, getComputerChoice())));
 
-
+/*
+playRound(getPlayerChoice(), getComputerChoice());
+*/
+/*
 function game() {
     let wins = 0;
     for (let i = 0; i < 5; i++) {
@@ -43,3 +67,4 @@ function game() {
 }
 
 game();
+*/
